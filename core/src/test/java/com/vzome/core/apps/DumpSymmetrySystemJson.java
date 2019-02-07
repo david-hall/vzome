77f13f223076a17f574e7ada087459402be5d2d9
David Hall <David.42.Hall@gmail.com>
2019-02-07 03:33:48 -0500

    Fix Compile Error

--- core/src/test/java/com/vzome/core/apps/DumpSymmetrySystemJson.java
+++ core/src/test/java/com/vzome/core/apps/DumpSymmetrySystemJson.java
@@ -4,7 +4,6 @@ import java.util.Properties;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.vzome.core.editor.FieldApplication;
 import com.vzome.core.editor.SymmetrySystem;
 import com.vzome.core.kinds.GoldenFieldApplication;
 import com.vzome.core.render.Colors;
@@ -13,7 +12,7 @@ public class DumpSymmetrySystemJson
 {
 	public static void main(String[] args)
 	{
-		FieldApplication app = new GoldenFieldApplication();
+		GoldenFieldApplication app = new GoldenFieldApplication();
 		Colors colors = new Colors( new Properties() );
 		SymmetrySystem system = new SymmetrySystem( null, app .getDefaultSymmetryPerspective(), null, colors, true );
 		ObjectMapper mapper = new ObjectMapper();
